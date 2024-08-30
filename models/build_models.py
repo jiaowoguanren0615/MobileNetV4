6f49eb0a434e8b52504aa39dd3fe274291b6be26
Huangshuqi <1105374939@qq.com>
2024-08-30 15:33:11 +1000

    Update model architecture

--- models/build_models.py
+++ models/build_models.py
@@ -1,12 +1,11 @@
 from timm.models import register_model
 
-from typing import Optional
+from typing import Any, Callable, Dict, List, Mapping, Optional, Tuple, Union
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-from models.model_config import MODEL_SPECS
+from model_config import MODEL_SPECS
 
 
 def make_divisible(
@@ -40,30 +39,30 @@ def make_divisible(
     return int(new_value)
 
 
-def conv_2d(inp, oup, kernel_size=3, stride=1, groups=1, bias=False, norm=True, act=True):
+def conv2d(in_channels, out_channels, kernel_size=3, stride=1, groups=1, bias=False, norm=True, act=True):
     conv = nn.Sequential()
     padding = (kernel_size - 1) // 2
-    conv.add_module('conv', nn.Conv2d(inp, oup, kernel_size, stride, padding, bias=bias, groups=groups))
+    conv.append(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=bias, groups=groups))
     if norm:
-        conv.add_module('BatchNorm2d', nn.BatchNorm2d(oup))
+        conv.append(nn.BatchNorm2d(out_channels))
     if act:
-        conv.add_module('Activation', nn.ReLU6())
+        conv.append(nn.ReLU6())
     return conv
 
 
 class InvertedResidual(nn.Module):
-    def __init__(self, inp, oup, stride, expand_ratio, act=False):
+    def __init__(self, in_channels, out_channels, stride, expand_ratio, act=False, squeeze_exactation=False):
         super(InvertedResidual, self).__init__()
         self.stride = stride
         assert stride in [1, 2]
-        hidden_dim = int(round(inp * expand_ratio))
+        hidden_dim = int(round(in_channels * expand_ratio))
         self.block = nn.Sequential()
         if expand_ratio != 1:
-            self.block.add_module('exp_1x1', conv_2d(inp, hidden_dim, kernel_size=1, stride=1))
-        self.block.add_module('conv_3x3',
-                              conv_2d(hidden_dim, hidden_dim, kernel_size=3, stride=stride, groups=hidden_dim))
-        self.block.add_module('red_1x1', conv_2d(hidden_dim, oup, kernel_size=1, stride=1, act=act))
-        self.use_res_connect = self.stride == 1 and inp == oup
+            self.block.add_module("exp_1x1", conv2d(in_channels, hidden_dim, kernel_size=3, stride=stride))
+        if squeeze_exactation:
+            self.block.add_module("conv_3x3", conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=stride, groups=hidden_dim))
+        self.block.add_module("res_1x1", conv2d(hidden_dim, out_channels, kernel_size=1, stride=1, act=act))
+        self.use_res_connect = self.stride == 1 and in_channels == out_channels
 
     def forward(self, x):
         if self.use_res_connect:
@@ -73,37 +72,31 @@ class InvertedResidual(nn.Module):
 
 
 class UniversalInvertedBottleneckBlock(nn.Module):
-    def __init__(self,
-                 inp,
-                 oup,
-                 start_dw_kernel_size,
-                 middle_dw_kernel_size,
-                 middle_dw_downsample,
-                 stride,
-                 expand_ratio
-                 ):
-        super().__init__()
-        # Starting depthwise conv.
+    def __init__(self, in_channels, out_channels, start_dw_kernel_size, middle_dw_kernel_size, middle_dw_downsample,
+                 stride, expand_ratio):
+        """An inverted bottleneck block with optional depthwises.
+        Referenced from here https://github.com/tensorflow/models/blob/master/official/vision/modeling/layers/nn_blocks.py
+        """
+        super(UniversalInvertedBottleneckBlock, self).__init__()
+        # starting depthwise conv
         self.start_dw_kernel_size = start_dw_kernel_size
         if self.start_dw_kernel_size:
             stride_ = stride if not middle_dw_downsample else 1
-            self._start_dw_ = conv_2d(inp, inp, kernel_size=start_dw_kernel_size, stride=stride_, groups=inp, act=False)
-        # Expansion with 1x1 convs.
-        expand_filters = make_divisible(inp * expand_ratio, 8)
-        self._expand_conv = conv_2d(inp, expand_filters, kernel_size=1)
-        # Middle depthwise conv.
+            self._start_dw_ = conv2d(in_channels, in_channels, kernel_size=start_dw_kernel_size, stride=stride_, groups=in_channels, act=False)
+        # expansion with 1x1 convs
+        expand_filters = make_divisible(in_channels * expand_ratio, 8)
+        self._expand_conv = conv2d(in_channels, expand_filters, kernel_size=1)
+        # middle depthwise conv
         self.middle_dw_kernel_size = middle_dw_kernel_size
         if self.middle_dw_kernel_size:
             stride_ = stride if middle_dw_downsample else 1
-            self._middle_dw = conv_2d(expand_filters, expand_filters, kernel_size=middle_dw_kernel_size, stride=stride_,
-                                      groups=expand_filters)
-        # Projection with 1x1 convs.
-        self._proj_conv = conv_2d(expand_filters, oup, kernel_size=1, stride=1, act=False)
+            self._middle_dw = conv2d(expand_filters, expand_filters, kernel_size=middle_dw_kernel_size, stride=stride_, groups=expand_filters)
+        # projection with 1x1 convs
+        self._proj_conv = conv2d(expand_filters, out_channels, kernel_size=1, stride=1, act=False)
 
-        # Ending depthwise conv.
-        # this not used
+        # expand depthwise conv (not used)
         # _end_dw_kernel_size = 0
-        # self._end_dw = conv_2d(oup, oup, kernel_size=_end_dw_kernel_size, stride=stride, groups=inp, act=False)
+        # self._end_dw = conv2d(out_channels, out_channels, kernel_size=_end_dw_kernel_size, stride=stride, groups=in_channels, act=False)
 
     def forward(self, x):
         if self.start_dw_kernel_size:
@@ -119,29 +112,178 @@ class UniversalInvertedBottleneckBlock(nn.Module):
         return x
 
 
+class MultiQueryAttentionLayerWithDownSampling(nn.Module):
+    def __init__(self, in_channels, num_heads, key_dim, value_dim, query_h_strides, query_w_strides, kv_strides, dw_kernel_size=3, dropout=0.0):
+        """Multi Query Attention with spatial downsampling.
+        Referenced from here https://github.com/tensorflow/models/blob/master/official/vision/modeling/layers/nn_blocks.py
+
+        3 parameters are introduced for the spatial downsampling:
+        1. kv_strides: downsampling factor on Key and Values only.
+        2. query_h_strides: vertical strides on Query only.
+        3. query_w_strides: horizontal strides on Query only.
+
+        This is an optimized version.
+        1. Projections in Attention is explict written out as 1x1 Conv2D.
+        2. Additional reshapes are introduced to bring a up to 3x speed up.
+        """
+        super(MultiQueryAttentionLayerWithDownSampling, self).__init__()
+        self.num_heads = num_heads
+        self.key_dim = key_dim
+        self.value_dim = value_dim
+        self.query_h_strides = query_h_strides
+        self.query_w_strides = query_w_strides
+        self.kv_strides = kv_strides
+        self.dw_kernel_size = dw_kernel_size
+        self.dropout = dropout
+
+        self.head_dim = self.key_dim // num_heads
+
+        if self.query_h_strides > 1 or self.query_w_strides > 1:
+            self._query_downsampling_norm = nn.BatchNorm2d(in_channels)
+        self._query_proj = conv2d(in_channels, self.num_heads * self.key_dim, 1, 1, norm=False, act=False)
+
+        if self.kv_strides > 1:
+            self._key_dw_conv = conv2d(in_channels, in_channels, dw_kernel_size, kv_strides, groups=in_channels,
+                                       norm=True, act=False)
+            self._value_dw_conv = conv2d(in_channels, in_channels, dw_kernel_size, kv_strides, groups=in_channels,
+                                         norm=True, act=False)
+        self._key_proj = conv2d(in_channels, key_dim, 1, 1, norm=False, act=False)
+        self._value_proj = conv2d(in_channels, key_dim, 1, 1, norm=False, act=False)
+        self._output_proj = conv2d(num_heads * key_dim, in_channels, 1, 1, norm=False, act=False)
+
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x):
+        bs, seq_len, _, _ = x.size()
+        # print(x.size())
+        if self.query_h_strides > 1 or self.query_w_strides > 1:
+            q = F.avg_pool2d(self.query_h_strides, self.query_w_strides)
+            q = self._query_downsampling_norm(q)
+            q = self._query_proj(q)
+        else:
+            q = self._query_proj(x)
+        px = q.size(2)
+        q = q.view(bs, self.num_heads, -1, self.key_dim)  # [batch_size, num_heads, seq_len, key_dim]
+
+        if self.kv_strides > 1:
+            k = self._key_dw_conv(x)
+            k = self._key_proj(k)
+            v = self._value_dw_conv(x)
+            v = self._value_proj(v)
+        else:
+            k = self._key_proj(x)
+            v = self._value_proj(x)
+        k = k.view(bs, 1, self.key_dim, -1)   # [batch_size, 1, key_dim, seq_length]
+        v = v.view(bs, 1, -1, self.key_dim)    # [batch_size, 1, seq_length, key_dim]
+
+        # calculate attention score
+        # print(q.shape, k.shape, v.shape)
+        attn_score = torch.matmul(q, k) / (self.head_dim ** 0.5)
+        attn_score = self.dropout(attn_score)
+        attn_score = F.softmax(attn_score, dim=-1)
+
+        # context = torch.einsum('bnhm,bmv->bnhv', attn_score, v)
+        # print(attn_score.shape, v.shape)
+        context = torch.matmul(attn_score, v)
+        context = context.view(bs, self.num_heads * self.key_dim, px, px)
+        output = self._output_proj(context)
+        # print(output.shape)
+        return output
+
+
+class MNV4layerScale(nn.Module):
+    def __init__(self, init_value):
+        """LayerScale as introduced in CaiT: https://arxiv.org/abs/2103.17239
+        Referenced from here https://github.com/tensorflow/models/blob/master/official/vision/modeling/layers/nn_blocks.py
+
+        As used in MobileNetV4.
+
+        Attributes:
+            init_value (float): value to initialize the diagonal matrix of LayerScale.
+        """
+        super(MNV4layerScale, self).__init__()
+        self.init_value = init_value
+
+    def forward(self, x):
+        gamma = self.init_value * torch.ones(x.size(-1), dtype=x.dtype, device=x.device)
+        return x * gamma
+
+
+class MultiHeadSelfAttentionBlock(nn.Module):
+    def __init__(self, in_channels, num_heads, key_dim, value_dim, query_h_strides, query_w_strides,
+                 kv_strides, use_layer_scale, use_multi_query, use_residual=True):
+        super(MultiHeadSelfAttentionBlock, self).__init__()
+        self.query_h_strides = query_h_strides
+        self.query_w_strides = query_w_strides
+        self.kv_strides = kv_strides
+        self.use_layer_scale = use_layer_scale
+        self.use_multi_query = use_multi_query
+        self.use_residual = use_residual
+        self._input_norm = nn.BatchNorm2d(in_channels)
+
+        if self.use_multi_query:
+            self.multi_query_attention = MultiQueryAttentionLayerWithDownSampling(
+                in_channels, num_heads, key_dim, value_dim, query_h_strides, query_w_strides, kv_strides
+            )
+        else:
+            self.multi_head_attention = nn.MultiheadAttention(in_channels, num_heads, kdim=key_dim)
+
+        if use_layer_scale:
+            self.layer_scale_init_value = 1e-5
+            self.layer_scale = MNV4layerScale(self.layer_scale_init_value)
+
+    def forward(self, x):
+        # Not using CPE, skipped
+        # input norm
+        shortcut = x
+        x = self._input_norm(x)
+        # multi query
+        if self.use_multi_query:
+            # print(x.size())
+            x = self.multi_query_attention(x)
+            # print(x.size())
+        else:
+            x = self.multi_head_attention(x, x)
+        # layer scale
+        if self.use_layer_scale:
+            x = self.layer_scale(x)
+        # use residual
+        if self.use_residual:
+            x = x + shortcut
+        return x
+
+
 def build_blocks(layer_spec):
-    if not layer_spec.get('block_name'):
+    global msha
+    if not layer_spec.get("block_name"):
         return nn.Sequential()
-    block_names = layer_spec['block_name']
+    block_names = layer_spec["block_name"]
     layers = nn.Sequential()
     if block_names == "convbn":
-        schema_ = ['inp', 'oup', 'kernel_size', 'stride']
-        args = {}
-        for i in range(layer_spec['num_blocks']):
-            args = dict(zip(schema_, layer_spec['block_specs'][i]))
-            layers.add_module(f"convbn_{i}", conv_2d(**args))
+        schema_ = ["in_channels", "out_channels", "kernel_size", "stride"]
+        for i in range(layer_spec["num_blocks"]):
+            args = dict(zip(schema_, layer_spec["block_specs"][i]))
+            layers.add_module(f"convbn_{i}", conv2d(**args))
     elif block_names == "uib":
-        schema_ = ['inp', 'oup', 'start_dw_kernel_size', 'middle_dw_kernel_size', 'middle_dw_downsample', 'stride',
-                   'expand_ratio']
-        args = {}
-        for i in range(layer_spec['num_blocks']):
-            args = dict(zip(schema_, layer_spec['block_specs'][i]))
+        schema_ = ["in_channels", "out_channels", "start_dw_kernel_size", "middle_dw_kernel_size", "middle_dw_downsample",
+                   "stride", "expand_ratio", "msha"]
+        for i in range(layer_spec["num_blocks"]):
+            args = dict(zip(schema_, layer_spec["block_specs"][i]))
+            msha = args.pop("msha") if "msha" in args else 0
             layers.add_module(f"uib_{i}", UniversalInvertedBottleneckBlock(**args))
+            if msha:
+                msha_schema_ = [
+                    "in_channels", "num_heads", "key_dim", "value_dim", "query_h_strides", "query_w_strides", "kv_strides",
+                    "use_layer_scale", "use_multi_query", "use_residual"
+                ]
+                args = dict(zip(msha_schema_, [args["out_channels"]] + (msha)))
+                layers.add_module(
+                    f"msha_{i}", MultiHeadSelfAttentionBlock(**args)
+                )
     elif block_names == "fused_ib":
-        schema_ = ['inp', 'oup', 'stride', 'expand_ratio', 'act']
-        args = {}
-        for i in range(layer_spec['num_blocks']):
-            args = dict(zip(schema_, layer_spec['block_specs'][i]))
+        schema_ = ["in_channels", "out_channels", "stride", "expand_ratio", "act"]
+        for i in range(layer_spec["num_blocks"]):
+            args = dict(zip(schema_, layer_spec["block_specs"][i]))
             layers.add_module(f"fused_ib_{i}", InvertedResidual(**args))
     else:
         raise NotImplementedError
@@ -149,7 +291,7 @@ def build_blocks(layer_spec):
 
 
 class MobileNetV4(nn.Module):
-    def __init__(self, model, num_classes=1000, **kwargs):
+    def __init__(self, model, num_classes=1000):
         # MobileNetV4ConvSmall  MobileNetV4ConvMedium  MobileNetV4ConvLarge
         # MobileNetV4HybridMedium  MobileNetV4HybridLarge
         """Params to initiate MobilenNetV4
@@ -157,32 +299,28 @@ class MobileNetV4(nn.Module):
             model : support 5 types of models as indicated in
             "https://github.com/tensorflow/models/blob/master/official/vision/modeling/backbones/mobilenet.py"
         """
-        super().__init__()
-        assert num_classes > 0, 'The number of classes must > 0!!!'
+        super(MobileNetV4, self).__init__()
+        # print(MODEL_SPECS.keys(), model not in MODEL_SPECS.keys())
         assert model in MODEL_SPECS.keys()
         self.model = model
         self.num_classes = num_classes
         self.spec = MODEL_SPECS[self.model]
 
         # conv0
-        self.conv0 = build_blocks(self.spec['conv0'])
+        self.conv0 = build_blocks(self.spec["conv0"])
         # layer1
-        self.layer1 = build_blocks(self.spec['layer1'])
+        self.layer1 = build_blocks(self.spec["layer1"])
         # layer2
-        self.layer2 = build_blocks(self.spec['layer2'])
+        self.layer2 = build_blocks(self.spec["layer2"])
         # layer3
-        self.layer3 = build_blocks(self.spec['layer3'])
+        self.layer3 = build_blocks(self.spec["layer3"])
         # layer4
-        self.layer4 = build_blocks(self.spec['layer4'])
+        self.layer4 = build_blocks(self.spec["layer4"])
         # layer5
-        self.layer5 = build_blocks(self.spec['layer5'])
+        self.layer5 = build_blocks(self.spec["layer5"])
 
-
-        self.last_layer_input_channel = self.spec['layer5']['block_specs'][-1][1]
-        # print(self.last_layer_input_channel)
-        self.head = nn.Linear(self.last_layer_input_channel, self.num_classes) if self.num_classes > 0 \
-            else nn.Identity()
-
+        # classify [optional]
+        self.head = nn.Linear(1280, num_classes)
 
     def forward(self, x):
         x0 = self.conv0(x)
@@ -192,11 +330,10 @@ class MobileNetV4(nn.Module):
         x4 = self.layer4(x3)
         x5 = self.layer5(x4)
         x5 = F.adaptive_avg_pool2d(x5, 1)
+        out = self.head(x5.flatten(1))
 
-        x5 = torch.flatten(x5, start_dim=1)
-        y = self.head(x5)
-        return y
         # return [x1, x2, x3, x4, x5]
+        return out
 
 
 
@@ -230,7 +367,7 @@ def mobilenetv4_hybrid_large(pretrained=False, pretrained_cfg=None, pretrained_c
 
 # if __name__ == '__main__':
 #     from torchinfo import summary
-#     model = mobilenetv4_hybrid_large()
+#     model = mobilenetv4_small()
 #     print("Check output shape ...")
 #     summary(model, input_size=(1, 3, 224, 224))
     # x = torch.rand(1, 3, 224, 224)
