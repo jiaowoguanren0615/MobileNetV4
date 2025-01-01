585e60512a0893fae937f2fba098fd04ee59351e
huangshuqi <1105374939@qq.com>
2025-01-01 10:31:04 +0800

    Update onnx_export code

--- onnx_export.py
+++ onnx_export.py
@@ -13,7 +13,9 @@ import torch
 import numpy as np
 import onnx
 import models
+from copy import deepcopy
 from timm.models import create_model
+from typing import Optional, Tuple, List
 
 
 
@@ -48,79 +50,188 @@ parser.add_argument('--aten-fallback', action='store_true', default=False,
                     help='Fallback to ATEN ops. Helps fix AdaptiveAvgPool issue with Caffe2 in newer PyTorch/ONNX.')
 parser.add_argument('--dynamic-size', action='store_true', default=False,
                     help='Export model width dynamic width/height. Not recommended for "tf" models with SAME padding.')
+parser.add_argument('--check-forward', action='store_true', default=False,
+                    help='Do a full check of torch vs onnx forward after export.')
 parser.add_argument('--mean', type=float, nargs='+', default=None, metavar='MEAN',
                     help='Override mean pixel value of datasets')
 parser.add_argument('--std', type=float,  nargs='+', default=None, metavar='STD',
                     help='Override std deviation of of datasets')
+parser.add_argument('--reparam', default=False, action='store_true',
+                    help='Reparameterize model')
+parser.add_argument('--training', default=False, action='store_true',
+                    help='Export in training mode (default is eval)')
+parser.add_argument('--verbose', default=False, action='store_true',
+                    help='Extra stdout output')
+parser.add_argument('--dynamo', default=False, action='store_true',
+                    help='Use torch dynamo export.')
 
 
+
+def reparameterize_model(model: torch.nn.Module, inplace=False) -> torch.nn.Module:
+    if not inplace:
+        model = deepcopy(model)
+
+    def _fuse(m):
+        for child_name, child in m.named_children():
+            if hasattr(child, 'fuse'):
+                setattr(m, child_name, child.fuse())
+            elif hasattr(child, "reparameterize"):
+                child.reparameterize()
+            elif hasattr(child, "switch_to_deploy"):
+                child.switch_to_deploy()
+            _fuse(child)
+
+    _fuse(model)
+    return model
+
+
+def onnx_forward(onnx_file, example_input):
+    import onnxruntime
+
+    sess_options = onnxruntime.SessionOptions()
+    session = onnxruntime.InferenceSession(onnx_file, sess_options)
+    input_name = session.get_inputs()[0].name
+    output = session.run([], {input_name: example_input.numpy()})
+    output = output[0]
+    return output
+
+
+def onnx_export(
+        model: torch.nn.Module,
+        output_file: str,
+        example_input: Optional[torch.Tensor] = None,
+        training: bool = False,
+        verbose: bool = False,
+        check: bool = True,
+        check_forward: bool = False,
+        batch_size: int = 64,
+        input_size: Tuple[int, int, int] = None,
+        opset: Optional[int] = None,
+        dynamic_size: bool = False,
+        aten_fallback: bool = False,
+        keep_initializers: Optional[bool] = None,
+        use_dynamo: bool = False,
+        input_names: List[str] = None,
+        output_names: List[str] = None,
+):
+    import onnx
+
+    if training:
+        training_mode = torch.onnx.TrainingMode.TRAINING
+        model.train()
+    else:
+        training_mode = torch.onnx.TrainingMode.EVAL
+        model.eval()
+
+    if example_input is None:
+        if not input_size:
+            assert hasattr(model, 'default_cfg')
+            input_size = model.default_cfg.get('input_size')
+        example_input = torch.randn((batch_size,) + input_size, requires_grad=training)
+
+    # Run model once before export trace, sets padding for models with Conv2dSameExport. This means
+    # that the padding for models with Conv2dSameExport (most models with tf_ prefix) is fixed for
+    # the input img_size specified in this script.
+
+    # Opset >= 11 should allow for dynamic padding, however I cannot get it to work due to
+    # issues in the tracing of the dynamic padding or errors attempting to export the model after jit
+    # scripting it (an approach that should work). Perhaps in a future PyTorch or ONNX versions...
+    with torch.no_grad():
+        original_out = model(example_input)
+
+    print("==> Exporting model to ONNX format at '{}'".format(output_file))
+
+    input_names = input_names or ["input0"]
+    output_names = output_names or ["output0"]
+
+    dynamic_axes = {'input0': {0: 'batch'}, 'output0': {0: 'batch'}}
+    if dynamic_size:
+        dynamic_axes['input0'][2] = 'height'
+        dynamic_axes['input0'][3] = 'width'
+
+    if aten_fallback:
+        export_type = torch.onnx.OperatorExportTypes.ONNX_ATEN_FALLBACK
+    else:
+        export_type = torch.onnx.OperatorExportTypes.ONNX
+
+    if use_dynamo:
+        export_options = torch.onnx.ExportOptions(dynamic_shapes=dynamic_size)
+        export_output = torch.onnx.dynamo_export(
+            model,
+            example_input,
+            export_options=export_options,
+        )
+        export_output.save(output_file)
+        torch_out = None
+    else:
+        #TODO, for torch version >= 2.5, use torch.onnx.export()
+        torch_out = torch.onnx._export(
+            model,
+            example_input,
+            output_file,
+            training=training_mode,
+            export_params=True,
+            verbose=verbose,
+            input_names=input_names,
+            output_names=output_names,
+            keep_initializers_as_inputs=keep_initializers,
+            dynamic_axes=dynamic_axes,
+            opset_version=opset,
+            operator_export_type=export_type
+        )
+
+    if check:
+        print("==> Loading and checking exported model from '{}'".format(output_file))
+        onnx_model = onnx.load(output_file)
+        onnx.checker.check_model(onnx_model, full_check=True)  # assuming throw on error
+        if check_forward and not training:
+            import numpy as np
+            onnx_out = onnx_forward(output_file, example_input)
+            if torch_out is not None:
+                np.testing.assert_almost_equal(torch_out.numpy(), onnx_out, decimal=3)
+                np.testing.assert_almost_equal(original_out.numpy(), torch_out.numpy(), decimal=5)
+            else:
+                np.testing.assert_almost_equal(original_out.numpy(), onnx_out, decimal=3)
 
 
 def main():
     args = parser.parse_args()
 
-    # args.pretrained = True
-    # if args.checkpoint:
-    #     args.pretrained = False
-
     if args.output == None:
         args.output = f'./{args.model}.onnx'
 
     print("==> Creating PyTorch {} model".format(args.model))
-    # NOTE exportable=True flag disables autofn/jit scripted activations and uses Conv2dSameExport layers
-    # for models using SAME padding
+
+
     model = create_model(
         args.model,
         num_classes=args.nb_classes,
         extra_attention_block=args.extra_attention_block,
-        # exportable=True
+        exportable=True
     )
 
     model.load_state_dict(torch.load(args.checkpoint)['model'])
     model.eval()
 
-    example_input = torch.randn((args.batch_size, 3, args.img_size or 224, args.img_size or 224), requires_grad=True)
+    if args.reparam:
+        model = reparameterize_model(model)
 
-    # Run model once before export trace, sets padding for models with Conv2dSameExport. This means
-    # that the padding for models with Conv2dSameExport (most models with tf_ prefix) is fixed for
-    # the input img_size specified in this script.
-    # Opset >= 11 should allow for dynamic padding, however I cannot get it to work due to
-    # issues in the tracing of the dynamic padding or errors attempting to export the model after jit
-    # scripting it (an approach that should work). Perhaps in a future PyTorch or ONNX versions...
-    model(example_input)
+    onnx_export(
+        model=model,
+        output_file=args.output,
+        opset=args.opset,
+        dynamic_size=args.dynamic_size,
+        aten_fallback=args.aten_fallback,
+        keep_initializers=args.keep_init,
+        check_forward=args.check_forward,
+        training=args.training,
+        verbose=args.verbose,
+        use_dynamo=args.dynamo,
+        input_size=(3, args.img_size, args.img_size),
+        batch_size=args.batch_size,
+    )
 
-    print("==> Exporting model to ONNX format at '{}'".format(args.output))
-    input_names = ["input0"]
-    output_names = ["output0"]
-    dynamic_axes = {'input0': {0: 'batch'}, 'output0': {0: 'batch'}}
-    if args.dynamic_size:
-        dynamic_axes['input0'][2] = 'height'
-        dynamic_axes['input0'][3] = 'width'
-    if args.aten_fallback:
-        export_type = torch.onnx.OperatorExportTypes.ONNX_ATEN_FALLBACK
-    else:
-        export_type = torch.onnx.OperatorExportTypes.ONNX
-
-    torch_out = torch.onnx._export(
-        model, example_input, args.output, export_params=True, verbose=True, input_names=input_names,
-        output_names=output_names, keep_initializers_as_inputs=args.keep_init, dynamic_axes=dynamic_axes,
-        opset_version=args.opset, operator_export_type=export_type)
-
-    print("==> Loading and checking exported model from '{}'".format(args.output))
-    onnx_model = onnx.load(args.output)
-    onnx.checker.check_model(onnx_model)  # assuming throw on error
     print("==> Passed")
-
-    if args.keep_init and args.aten_fallback:
-        import caffe2.python.onnx.backend as onnx_caffe2
-        # Caffe2 loading only works properly in newer PyTorch/ONNX combos when
-        # keep_initializers_as_inputs and aten_fallback are set to True.
-        print("==> Loading model into Caffe2 backend and comparing forward pass.".format(args.output))
-        caffe2_backend = onnx_caffe2.prepare(onnx_model)
-        B = {onnx_model.graph.input[0].name: x.data.numpy()}
-        c2_out = caffe2_backend.run(B)[0]
-        np.testing.assert_almost_equal(torch_out.data.numpy(), c2_out, decimal=5)
-        print("==> Passed")
 
 
 if __name__ == '__main__':
